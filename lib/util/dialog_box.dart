e91ebba11a31fc87afeacd5369128fa3404c5c47
Jana <janakamberi4@gmail.com>
2025-02-06 10:39:46 +0100

    First Drop

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import 'my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+
+  DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.deepPurple[300],
+      content: Container(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // get user input
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a new task",
+              ),
+            ),
+
+            // buttons -> save + cancel
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // save button
+                MyButton(
+                  text: "Save",
+                  onPressed: () {
+                    // Check if the text field is empty
+                    if (controller.text.trim().isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Task cannot be empty!"),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    } else {
+                      // Save the task
+                      onSave();
+                    }
+                  },
+                ),
+
+                const SizedBox(width: 8),
+
+                // cancel button
+                MyButton(text: "Cancel", onPressed: onCancel),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
